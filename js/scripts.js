29fbf9a8bf1bc59b6719412b59b000dbaa014b6c
Gavin Kimberlin and Josh Mabry <gavin@>
2019-06-04 10:59:16 -0700

    q git add . git-pair-commit -m

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
   $(".night-toggle").click(function(){
     $("body").removeClass();
     $("body").addClass("night-mode");
-
-  }); 
+  });
+  $(".day-toggle").click(function() {
+    $("body").removeClass();
+  });
+  $(".highlight").click(function() {
+    $(".highlight").addClass("highlight-paragraph")
+  })
 });
